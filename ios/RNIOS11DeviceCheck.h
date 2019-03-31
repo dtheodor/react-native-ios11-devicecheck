25e65fb06f6f209369b466cac9a0ad29260e6b0c
Dimitris Theodorou <dimitris.theodorou@gmail.com>
2019-03-31 19:24:13 +0300

    Update RNIOS11DeviceCheck.h

--- ios/RNIOS11DeviceCheck.h
+++ ios/RNIOS11DeviceCheck.h
@@ -3,7 +3,10 @@
 // All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 
 @interface RNIOS11DeviceCheck : NSObject <RCTBridgeModule> @end
